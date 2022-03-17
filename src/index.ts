0bdfac0f1551a250e4039f51162486aaae26a853
King Larce Cortes <kingcort01@gmail.com>
2022-03-18 07:36:11 +0800

    build: Add express and typescript watcher

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-function test(a: number) {
-  console.log(`Hello World ${a}`);
-}
-test(101);
+import express, { Application } from "express";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
+
+app.get("/ping", async (_req, res) => {
+  res.send({
+    message: "pong",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
