0c505f5275352ee45f240dc3bc244f6946187f3b
King Larce Cortes <kingcort01@gmail.com>
2022-03-18 08:50:45 +0800

    build: Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import morgan from "morgan";
 import Router from "./routes";
+import cors from "cors";
 
 const PORT = process.env.PORT || 8000;
 
@@ -9,6 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
+app.use(cors());
 app.use(Router);
 
 app.get("/ping", async (_req, res) => {
